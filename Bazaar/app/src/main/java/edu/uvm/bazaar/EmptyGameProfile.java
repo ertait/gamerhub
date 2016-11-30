5c21a39c1a0323d8ec167fe07ea87daefd1eccdc
Amanda <stormwing95@gmail.com>
2016-11-30 11:47:41 -0500

    adding the games list and game page

---
+++ Bazaar/app/src/main/java/edu/uvm/bazaar/EmptyGameProfile.java
@@ -0,0 +1,18 @@
+package edu.uvm.bazaar;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+public class EmptyGameProfile extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_empty_game_profile);
+
+    }
+
+}
