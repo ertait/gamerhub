79e26902a739800a448026cefba4b05f5fc736a2
Amanda <stormwing95@gmail.com>
2016-12-11 18:01:33 -0500

    the list of threads and the working thread forum

--- Bazaar/app/src/main/java/edu/uvm/bazaar/EmptyGameProfile.java
+++ Bazaar/app/src/main/java/edu/uvm/bazaar/EmptyGameProfile.java
@@ -1,8 +1,11 @@
 package edu.uvm.bazaar;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 
 import edu.uvm.loginregister.R;
@@ -14,6 +17,16 @@ public class EmptyGameProfile extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_empty_game_profile);
+
+        final Button threadsbutton = (Button)findViewById(R.id.threadsbutton);
+        threadsbutton.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View view){
+                Intent intent = new Intent(EmptyGameProfile.this, ThreadList.class);
+                startActivity(intent);
+            }
+        });
 
     }
 
---
+++ Bazaar/app/src/main/java/edu/uvm/bazaar/ThreadList.java
@@ -0,0 +1,40 @@
+package edu.uvm.bazaar;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import edu.uvm.loginregister.R;
+
+public class ThreadList extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_thread_list);
+
+        ListView listView =(ListView) findViewById(R.id.list);
+
+        String[] values = new String[]{"Thread1","Thread2","Thread3","Etc."};
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,android.R.id.text1,values);
+
+        listView.setAdapter(adapter);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            public void onItemClick(AdapterView<?> parent, View v,
+                                    int position, long id) {
+                //Toast.makeText(main.this, "" + position,
+                //Toast.LENGTH_SHORT).show();
+                Intent intent = new Intent(ThreadList.this, Thread.class);
+                startActivity(intent);
+
+            }
+        });
+
+
+    }
+}
+
